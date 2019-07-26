5827990724ab7a4c95fb35f224bed2a81c3969ea
Joseph Kim <josepheh.kim@gmail.com>
2019-07-26 03:31:07 -0400

    assembling the header component

---
+++ sprint-1/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../stylings/Header/scss';
+import App from './App';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Hello World!</div>
+      </>  
+    );
+  }
+}
+export default App;
