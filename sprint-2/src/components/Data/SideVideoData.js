ecbbc471cd97e98b41f1ae9177af886ae056e379
Joseph Kim <josepheh.kim@gmail.com>
2019-07-31 10:13:18 -0400

    first sprint 2 commit

---
+++ sprint-2/src/components/Data/SideVideoData.js
@@ -0,0 +1,52 @@
+import SideVideo1 from '../../Assets/Images/video-list-1.jpg';
+import SideVideo2 from '../../Assets/Images/video-list-2.jpg';
+import SideVideo3 from '../../Assets/Images/video-list-3.jpg';
+import SideVideo4 from '../../Assets/Images/video-list-4.jpg';
+import SideVideo5 from '../../Assets/Images/video-list-5.jpg';
+import SideVideo6 from '../../Assets/Images/video-list-6.jpg';
+import SideVideo7 from '../../Assets/Images/video-list-7.jpg';
+import SideVideo8 from '../../Assets/Images/video-list-8.jpg';
+
+const sideVideoData = [{
+  id: 'video-list-1',
+  title: 'Become A Travel Pro In One Easy Lesson',
+  channel: 'Todd Welch',
+  image: {SideVideo1}
+}, {
+  id: 'video-list-2',
+  title: 'Les Houches The Hidden Gem Of The Chamonix',
+  channel: 'Cornelia Blair',
+  image: {SideVideo2}
+}, {
+  id: 'video-list-3',
+  title: 'Travel Health Useful Medical Information For',
+  channel: 'Glenn Harper',
+  image: {SideVideo3}
+}, {
+  id: 'video-list-4',
+  title: 'Cheap Airline Tickets Great Ways To Save',
+  channel: 'Emily Harper',
+  image: {SideVideo4}
+}, {
+  id: 'video-list-5',
+  title: 'Take A Romantic Break In A Boutique Hotel',
+  channel: 'Ethan Owen',
+  image: {SideVideo5}
+}, {
+  id: 'video-list-6',
+  title: 'Choose The Perfect Accommodations',
+  channel: 'Lydia Perez',
+  image: {SideVideo6}
+}, {
+  id: 'video-list-7',
+  title: 'Cruising Destination Ideas',
+  channel: 'Timothy Austin',
+  image: {SideVideo7}
+}, {
+  id: 'video-list-8',
+  title: 'Train Travel On Track For Safety',
+  channel: 'Scotty Cranmer',
+  image: {SideVideo8}
+}];
+
+export default sideVideoData;
---
+++ sprint-2/src/components/VideoSection/VideoSection.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import './VideoSection.scss';
+//import MainVideo from '../../Assets/Video/BrainStation\ \Sample\ \Video.mp4';
+import Poster from '../../Assets/Images/video-list-0.jpg';
+import IconViews from '../../Assets/Icons/PNG/Icon-views.png';
+import IconLikes from '../../Assets/Icons/PNG/Icon-likes.png';
+
+class VideoSection extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      MainVideoData: props.MainVideoData
+    }
+  }
+  render() {
+    //console.log("helloworl" + this.state.MainVideoData.id);
+    //console.log("helloworl" + this.state.MainVideoData.views);
+    return (
+      <>
+        <div className = "video-section">
+          <div className = "video-section__video">
+            {/*<p>{this.props.MainVideoData.id}</p>*/}
+            {/*<p>{this.state.MainVideoData.id}</p>*/}
+            <VideoSectionVideo video = {this.state.MainVideoData.video}/>
+          </div>
+          <div className = "video-section__description">
+            <VideoSectionDescription MainVideoData = {this.state.MainVideoData} />
+          </div>
+        </div>
+      </>  
+    );
+  }
+}
+class VideoSectionVideo extends React.Component {
+  render () {
+    return (
+      <>
+        <video controls 
+          width = "100%"
+          //autoPlay
+          poster = {Poster}
+          src = {this.props.video}  
+          type = "video/mp4">
+        </video>
+      </>
+    )
+  }
+}
+class VideoSectionDescription extends React.Component {
+  render() {
+    return (
+      <>
+        <div className= "video-section__information__title">
+          <h2>{this.props.MainVideoData.title}</h2>
+        </div>
+        <div className= "video-section__information__stats">
+          <div className= "video-section__information__stats__name-date">
+            <ul>
+              <li className = "video-section__uploader">
+                {this.props.MainVideoData.channel}
+              </li>
+              <li className = "video-section__date">
+              {this.props.MainVideoData.timestamp}
+              </li>
+            </ul>
+          </div>
+          <div className= "video-section__information__stats__views-likes">
+            <ul>
+              <li className = "video-section__views">
+                <img src = {IconViews}></img>
+                {this.props.MainVideoData.views}
+              </li>
+              <li className = "video-section__likes">
+                <img src = {IconLikes}></img>
+                {this.props.MainVideoData.likes}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className= "video-section__information__description">
+          <p>
+          {this.props.MainVideoData.description}
+          </p>
+        
+        </div>
+      </>  
+    );
+  }
+}
+export default VideoSection;
